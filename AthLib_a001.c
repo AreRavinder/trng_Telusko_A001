f7e3611129d8d114aead9e3360489986a1541b08
AthVsh <amruth.kv@hotmail.com>
2018-01-08 11:58:22 +0000

    //Function-A001; long double func_A001_add(double a, double b)

--- AthLib_a001.c
+++ AthLib_a001.c
@@ -1,1 +1,11 @@
 //AthLib_a001.c
+
+
+//Function-A001
+long double func_A001_add(double a, double b)
+{
+ long double result;
+ result = (long double) (a) +  (long double) (b);
+  
+ return result; 
+}
